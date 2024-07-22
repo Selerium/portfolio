30b765cc21ec281571b99b057d3a04fa12e4ba79
John Adithya <jadithya@42student.abudhabi.ae>
2024-07-22 19:26:06 +0400

    responsive for table + mobile, needs for small laptops

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
 import './style.scss';
 import * as THREE from 'three';
-import { render } from 'sass';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -140,13 +139,141 @@ document.querySelector('#app').innerHTML = `
     <div class="info-section">
       <h1>projects i've worked on</h1>
       <div class="resume">
+      <button id="about-me">about me</button>
+      </div>
+    </div>
+    <div class="about">
+      <div class="top-section">
         <button id="back">back</button>
-        <button id="about-me">about me</button>
-        <h1><a href="CV_JohnAdi.pdf" download>my resume</a></h1>
+        <br>
+        <div class="grid">
+          <div class="me">
+            <img>
+            <h1>about me.</h1>
+          </div>
+          <p>An <b>ambitious, hardworking creative</b>, pursuing a career in <b>software development</b>. Highly proficient in web dev, across a variety of frameworks and platforms. Development of dynamic and static sites alongside <b>high quality UI/UX design skills</b>. Exceptional quality of work individually as well as within a team in both participative and managerial roles. <b>Extroverted, bold, cross-culture personality</b> – open-minded and ready for any kind of situation. <b>Easy to adapt</b> and <b>quick to learn</b> additional skills as required by the job.</p>
+        </div>
+        <div class="h1-wrap">
+          <h1 class="button-text"><a href="CV_JohnAdi.pdf" download>my resume</a></h1>
+        </div>
+        <img id="down" src="down-arrow.png">
+      </div>
+      <div class="jobs">
+        <h1 class="button-text">my career</h1>
+        <div class="job-wrap">
+          <img>
+          <div class="line"></div>
+          <div class="job-info">
+            <h2>adi.</h2>
+            <h3>(my personal brand) - freelance</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin luctus leo non enim vulputate, non sollicitudin urna malesuada. Aliquam erat volutpat. Quisque quis nibh lacus. Nulla suscipit accumsan ipsum eu molestie. Fusce lorem velit, viverra vitae velit at, sagittis imperdiet augue.</p>
+          </div>
+        </div>
+        <div class="job-wrap">
+          <img>
+          <div class="line"></div>
+          <div class="job-info">
+            <h2>Kalvad DevOps</h2>
+            <h3>(since 2023) - software engineer</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin luctus leo non enim vulputate, non sollicitudin urna malesuada. Aliquam erat volutpat. Quisque quis nibh lacus. Nulla suscipit accumsan ipsum eu molestie. Fusce lorem velit, viverra vitae velit at, sagittis imperdiet augue.</p>
+          </div>
+        </div>
+        <div class="job-wrap">
+          <img>
+          <div class="line"></div>
+          <div class="job-info">
+            <h2>Print&Go</h2>
+            <h3>(since 2020) - advisory</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin luctus leo non enim vulputate, non sollicitudin urna malesuada. Aliquam erat volutpat. Quisque quis nibh lacus. Nulla suscipit accumsan ipsum eu molestie. Fusce lorem velit, viverra vitae velit at, sagittis imperdiet augue.</p>
+          </div>
+        </div>
+      </div>
+      <div class="jobs">
+        <h1 class="button-text">my skills</h1>
+        <div class="all-skills">
+          <div class="skill-wrap">
+            <h2>Web Development</h2>
+            <div class="skill-info">
+              <p>HTML</p>
+              <p>CSS</p>
+              <p>JavaScript</p>
+              <p>TypeScript</p>
+              <p>ThreeJS</p>
+              <p>ViteJS</p>
+              <p>ParcelJS</p>
+              <p>PugJS</p>
+              <p>Zola</p>
+              <p>Hugo</p>
+            </div>
+          </div>
+
+          <div class="line"></div>
+
+          <div class="skill-wrap">
+            <h2>Software Development</h2>
+            <div class="skill-info">
+              <p>C/C++</p>
+              <p>Shell</p>
+              <p>Java</p>
+              <p>Electron</p>
+              <p>Git</p>
+              <p>Strapi</p>
+              <p>Payload</p>
+              <p>Ghost</p>
+              <p>SQL</p>
+            </div>
+          </div>
+
+          <div class="line"></div>
+
+          <div class="skill-wrap">
+            <h2>Creativity & Design</h2>
+            <div class="skill-info">
+              <p>Figma</p>
+              <p>Canva</p>
+              <p>Gimp</p>
+              <p>DaVinci Resolve</p>
+              <p>Unity/Unreal</p>
+            </div>
+          </div>
+
+          <div class="skill-wrap">
+            <h2>Project Management</h2>
+            <div class="skill-info">
+              <p>Taiga</p>
+              <p>GitHub Projects</p>
+              <p>Notion</p>
+              <p>Microsoft Excel/Google Sheets</p>
+            </div>
+          </div>
+
+          <div class="line"></div>
+
+          <div class="skill-wrap">
+            <h2>Qualifications</h2>
+            <div class="skill-info">
+              <p>CCNA</p>
+              <p>GitHub Projects</p>
+              <p>Notion</p>
+              <p>Microsoft Excel/Google Sheets</p>
+            </div>
+          </div>
+
+          <div class="line"></div>
+
+          <div class="skill-wrap">
+            <h2>Education</h2>
+            <div class="skill-info">
+              <p>42AbuDhabi</p>
+              <p>Canadian University Dubai</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </main>
     `
+      // <img id="up" src="up-arrow.png">
 
 function changeBackground(color) {
   document.getElementById('gradient-layer').style.backgroundImage = color;
@@ -188,16 +315,32 @@ function init() {
     document.querySelector('.hero-section').style.opacity = 0;
     document.querySelector('.info-section').style.opacity = 1;
     renderer.domElement.style.opacity = 1;
-    resetButton.style.opacity = 1;
-    resetButton.style.bottom = "2rem";
+    // resetButton.style.opacity = 1;
+    // resetButton.style.bottom = "2rem";
   });
 
   document.querySelector('#about-me').addEventListener('click', () => {
     window.scrollTo(0, window.innerHeight);
+    // document.querySelector('header').style.mixBlendMode = 'difference';
+    // document.querySelector('canvas').style.opacity = 0;
   });
-
+  
   document.querySelector('#back').addEventListener('click', () => {
     window.scrollTo(0, 0);
+    // document.querySelector('header').style.mixBlendMode = '';
+    // document.querySelector('canvas').style.opacity = 1;
+  });
+
+  document.querySelector('#down').addEventListener('click', () => {
+    window.scrollTo(0, window.innerHeight * 2);
+  });
+
+  document.querySelector('#up').addEventListener('click', () => {
+    window.scrollTo(0, window.innerHeight);
+  });
+
+  document.body.onscroll(() => {
+    console.log('scrolling');
   });
 }
 
@@ -206,4 +349,11 @@ function onWindowResize() {
   camera.updateProjectionMatrix();
   renderer.setSize( window.innerWidth, window.innerHeight );
 }
-window.addEventListener( 'resize', onWindowResize, false );
+window.addEventListener('resize', onWindowResize, false );
+
+window.onbeforeunload = () => {
+  document.documentElement.style.scrollBehavior = 'auto';
+  window.scrollTo(0, 0);
+  document.documentElement.scrollTop();
+  document.documentElement.style.scrollBehavior = 'smooth';
+};
