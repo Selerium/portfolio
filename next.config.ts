1eee228f5e0a72100b348a4a335b10ef3931d70a
Selerium <johnadithya008@gmail.com>
2025-05-26 02:48:36 +0400

    adding output export

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const isGithubPages = process.env.GITHUB_PAGES === 'true';
 const nextConfig: NextConfig = {
   basePath: isGithubPages ? '/portfolio' : '',
   assetPrefix: isGithubPages ? '/portfolio/' : '',
+  output: "export",
+  trailingSlash: true
 };
 
 export default nextConfig;
