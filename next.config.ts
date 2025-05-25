b62be1bcb113b9cb3541c778cf8e8c31f3338c00
Selerium <johnadithya008@gmail.com>
2025-05-26 02:38:52 +0400

    added actual repo name lol

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const isGithubPages = process.env.GITHUB_PAGES === 'true';
 
 const nextConfig: NextConfig = {
-  basePath: isGithubPages ? '/your-repo-name' : '',
-  assetPrefix: isGithubPages ? '/your-repo-name/' : '',
+  basePath: isGithubPages ? '/portfolio' : '',
+  assetPrefix: isGithubPages ? '/portfolio/' : '',
 };
 
 export default nextConfig;
