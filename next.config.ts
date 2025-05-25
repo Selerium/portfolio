0bdd916f5921a5d3fdeb25d6bbdfcc3ab7c104d4
Selerium <johnadithya008@gmail.com>
2025-05-26 02:32:13 +0400

    adding githubpages check

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: isGithubPages ? '/your-repo-name' : '',
+  assetPrefix: isGithubPages ? '/your-repo-name/' : '',
 };
 
 export default nextConfig;
