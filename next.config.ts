7a8afb9a1f2fa6d77f13cdf6fd4791d0f66006f4
Selerium <johnadithya008@gmail.com>
2025-05-26 02:54:00 +0400

    checking

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const isGithubPages = process.env.GITHUB_PAGES === 'true';
 
+console.log(`process.env.GITHUB_PAGES: ${isGithubPages}`);
+
 const nextConfig: NextConfig = {
   basePath: isGithubPages ? '/portfolio' : '',
   assetPrefix: isGithubPages ? '/portfolio/' : '',
